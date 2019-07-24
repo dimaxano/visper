f5eec0cfc02c510da611eb2913ff75d55317c03e
Dmitry Klimenkov <dimm.klimenkov@gmail.com>
2019-07-24 17:34:09 +0300

    added more external configurables

--- visper/utils.py
+++ visper/utils.py
@@ -1,7 +1,14 @@
 import gin
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.optim import Adam
 
 def add_external_configurables():
     #pass
     from .models.utils import get_class_weight
     gin.external_configurable(get_class_weight)
+
+    gin.external_configurable(ReduceLROnPlateau)
+
+    gin.external_configurable(Adam, blacklist=["params", "lr", "weight_decay"])
+
 
