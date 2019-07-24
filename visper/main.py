18ade3a9d30d8ef0d8dd77bb95845788c38080b4
Dmitry Klimenkov <dimm.klimenkov@gmail.com>
2019-07-24 17:36:11 +0300

    integration with gin improved

--- visper/main.py
+++ visper/main.py
@@ -1,17 +1,19 @@
-
 import gin
 import torch
 from torch.utils.data import DataLoader
 from catalyst.dl import SupervisedRunner
-from catalyst.dl.callbacks.metrics import AccuracyCallback
+from catalyst.dl.callbacks import AccuracyCallback, LRFinder
+from catalyst.dl.callbacks import InferCallback, VerboseLogger
 
 from .models import LipNext
 from .data.dataset import LipreadingDataset
 from .utils import add_external_configurables
-from .callbacks import CheckpointCallbackV2
+from .callbacks import CheckpointCallbackV2, NegativeMiningCallback
 
-@gin.configurable(blacklist=[])
+
+@gin.configurable(blacklist=["config_path"])
 def main(
+    config_path,
     log_dir=None,
     experiment_name=None,
     dataset_dir=None,
@@ -21,10 +23,16 @@ def main(
     num_workers=4,
     val_batch_size_multiplier=2,
     lr=1e-3,
+    scheduler=None,
+    optimizer=None,
     weight_decay=0,
     class_weight=None,
     check=False,
-    verbose=True):
+    verbose=True,
+    cudnn_benchmark=True):
+
+    if torch.cuda.is_available() and cudnn_benchmark:
+        torch.backends.cudnn.benchmark = True
     
     # experiment setup
     logdir = log_dir + experiment_name
@@ -57,20 +65,24 @@ def main(
     model = LipNext()
 
     criterion = torch.nn.CrossEntropyLoss(weight=class_weight)
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer)
-
+    optimizer = optimizer(model.parameters(), lr=lr, weight_decay=weight_decay)
+    if scheduler:
+        scheduler = scheduler(optimizer)
+    else:
+        scheduler = scheduler
+    
     # model runner
     device = torch.device("cpu")
     if torch.cuda.is_available():
         device = torch.device("cuda")
-    runner = SupervisedRunner(device=device)
 
+    runner = SupervisedRunner(device=device)
 
     # callbacks
     acc_callback = AccuracyCallback(accuracy_args=[1, 3])
-    ckpt_callback = CheckpointCallbackV2()
-    callbacks = [acc_callback, ckpt_callback]
+    ckpt_callback = CheckpointCallbackV2(config_path=config_path)
+    neg_mining_callback = NegativeMiningCallback()
+    callbacks = [acc_callback, ckpt_callback, neg_mining_callback]
 
 
     # model training
@@ -91,8 +103,10 @@ def main(
 
 
 if __name__ == "__main__":
+    CONFIG_PATH = "./main_config.gin"
+
     add_external_configurables()
-    gin.parse_config_file("config.gin", skip_unknown=False)
+    gin.parse_config_file(CONFIG_PATH, skip_unknown=False)
     
 
-    main()
+    main(CONFIG_PATH)
